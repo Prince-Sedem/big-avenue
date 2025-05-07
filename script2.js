deb61e4c2ac903749dd92ee0e743c83116982720
Prince-Sedem <sedemdev@gmail.com>
2025-05-07 16:45:27 +0100

    Update with number

--- script2.js
+++ script2.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const productPrice = button.getAttribute('data-price');
             
             // Construct the WhatsApp link
-            const phoneNumber = "23324755211";  // Your WhatsApp number
+            const phoneNumber = "233247552111";  // Your WhatsApp number
             const message = `Hi, I'm interested in buying the ${productName} for ${productPrice}.`;
             const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
             
